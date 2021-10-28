310bcd45939bce0a8fcf35ec70cacd1e90bfb272
ayanmoldagaliev <46749831+ayanmoldagaliev@users.noreply.github.com>
2021-10-28 23:50:20 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php 
+  include "includes/header.php"; 
+
+  if (isset($_GET['url']) && !empty($_GET['url'])) {
+    $url = strtolower(trim($_GET['url']));
+
+    $link = db_query("SELECT * FROM `links` WHERE `short_link` = '$url';")->fetch();
+    if (empty($link)){
+      echo "такая ссылка не найдена!";
+      die;
+    }
+
+    db_exec("UPDATE `links` SET `views` = `views` + 1 WHERE `short_link` = '$url';");
+    header('Location: ' . $link['long_link']) ;
+    die;
+  }
+?>
+<main class="container">
+  <div class="row mt-5">
+    <div class="col">
+      <h2 class="text-center">
+        Необходимо <a href="<?php echo get_url('register.php');?>">зарегистрироваться</a> или
+        <a href="<?php echo get_url('login.php');?>">войти</a> под своей учетной записью
+      </h2>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col">
+      <h2 class="text-center">Пользователей в системе: <?php echo $users_count; ?></h2>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col">
+      <h2 class="text-center">Ссылок в системе: <?php echo $link_count ?></h2>
+    </div>
+  </div>
+  <div class="row mt-5">
+    <div class="col">
+      <h2 class="text-center">Всего переходов по ссылкам: <?php echo $views_count ?></h2>
+    </div>
+  </div>
+</main>
+<?php include "includes/footer.php"; ?>

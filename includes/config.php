f746a46b5414a6c5428c9f10c7f5b8167dc6b28a
ayanmoldagaliev <46749831+ayanmoldagaliev@users.noreply.github.com>
2021-10-29 18:30:23 +0600

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+define('SITE_NAME', "Cut your URL");
+define('HOST', "http://" . $_SERVER['HTTP_HOST']);
+
+define('DB_HOST', "127.0.0.1");
+define('DB_NAME', "cut_u_url");
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+session_start();
---
+++ includes/functions.php
@@ -0,0 +1,114 @@
+<?php
+
+include "includes/config.php";
+
+function get_url($page = '') {
+  return HOST . "/$page";
+}
+
+function db() {
+  try {
+    return new PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME . "; charset=utf8", DB_USER, DB_PASS, [
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ]);
+  } catch (PDOException $e) {
+    die($e->getMessage());
+  }
+}
+
+function db_query($sql = '', $exec = false){
+  if (empty($sql)) return false;
+
+  if($exec) {
+    return db()->exec($sql);
+  }
+  return db()->query($sql);
+}
+
+function get_users_count() {
+  return db_query("SELECT COUNT(id) FROM `users`;")->fetchColumn();
+}
+
+function get_views_count() {
+  return db_query("SELECT SUM(`views`) FROM `links`")->fetchColumn();
+}
+
+function get_links_count() {
+  return db_query("SELECT COUNT(id) FROM `links`")->fetchColumn();
+}
+
+function get_link_info($url) {
+  if (empty($url)) return [];
+
+  return db_query("SELECT * FROM `links` WHERE `short_link` = '$url';")->fetch();
+}
+
+function get_user_info($login) {
+  if (empty($login)) return [];
+
+  return db_query("SELECT * FROM `users` WHERE `login` = '$login';")->fetch();
+}
+function update_views($url) {
+  if (empty($url)) return false;
+  return db_query("UPDATE `links` SET `views` = `views` + 1 WHERE `short_link` = '$url';", true);
+}
+
+function add_user($login, $pass) {
+  $password = password_hash($pass, PASSWORD_DEFAULT);
+
+  return db_query("INSERT INTO `users` (`id`, `login`, `password`) VALUES (NULL,' $login', '$password');", true);
+}
+
+function register_user($auth_data) {
+  if (empty($auth_data) || !isset($auth_data['login']) || empty($auth_data['login']) || !isset($auth_data['pass']) || !isset($auth_data['pass2'])) return false;
+
+  $user = get_user_info($auth_data['login']);
+
+  if (!empty($user)){
+    $_SESSION['error'] = "Пользователь '" . $auth_data['login'] . "' уже существует";
+    header('Location: register.php');
+    die;
+  }
+
+  if ($auth_data['pass'] !== $auth_data['pass2']) {
+    $_SESSION['error'] = "Пароли не совподают";
+    header('Location: register.php');
+    die;
+  }
+  if (add_user($auth_data['login'], $auth_data['pass'])) {
+    $_SESSION['success'] = "Регистрация прошла успешно";
+    header('Location: login.php');
+    die;
+  }
+  return true;
+}
+
+function login($auth_data) {
+  if (empty($auth_data) || !isset($auth_data['login']) || empty($auth_data['login']) || !isset($auth_data['pass']) || empty($auth_data['pass'])) {
+    $_SESSION['error'] = "Логин или пароль не может быть пустым";
+    header('Location: login.php');
+    die;
+  }
+
+  $user = get_user_info($auth_data['login']);
+  var_dump($user);
+  if (empty($user)) {
+    $_SESSION['error'] = "Логин или пароль неверный";
+    header('Location: login.php');
+    die;
+  }
+
+  if (password_verify($auth_data['pass'], $user['password'])) {
+    $_SESSION['user'] = $user;
+
+    header('Location: profile.php');
+    die;
+  } else {
+    $_SESSION['error'] = "Логин или пароль неверный";
+
+    header('Location: login.php');
+    die;
+  }
+}
